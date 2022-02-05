942e16879085b38ba942ac9d19321f56c96b2a50
Clara Gaset <claragaset4@gmail.com>
2022-02-05 11:02:52 +0100

    Armstrong number refactored

--- armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
+++ armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -1,9 +1,10 @@
 import scala.math.pow
 
 object ArmstrongNumbers:
-  def isArmstrongNumber(number: Int) =
-    val length = number.toString.length
-    val numberAsList: List[Double] = number.toString.map(_.asDigit.toDouble).toList
-    val sumOfNumbers = numberAsList.fold[Double](0)((x, y) => x + pow(y, length))
-    if sumOfNumbers.toInt == number then true
-    else false
+
+  def isArmstrongNumber(number: Int) = {
+    val numberAsString = number.toString
+    val length = numberAsString.length
+
+    numberAsString.map(_.asDigit).fold(0)((x, y) => x + pow(y, length).toInt) == number
+  }
