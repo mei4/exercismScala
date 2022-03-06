6d6903f952cec0f271e7c31f6581d1a27add0e57
Clara Gaset <claragaset4@gmail.com>
2022-03-06 11:45:38 +0100

    House: removes loop to write paragraph

--- house/src/main/scala/House.scala
+++ house/src/main/scala/House.scala
@@ -1,27 +1,25 @@
 object House:
   val phrases = List(
     "the house that Jack built.",
-    "the malt that lay in",
-    "the rat that ate",
-    "the cat that killed",
-    "the dog that worried",
-    "the cow with the crumpled horn that tossed",
-    "the maiden all forlorn that milked",
-    "the man all tattered and torn that kissed",
-    "the priest all shaven and shorn that married",
-    "the rooster that crowed in the morn that woke",
-    "the farmer sowing his corn that kept",
-    "the horse and the hound and the horn that belonged to"
+    "the malt that lay in ",
+    "the rat that ate ",
+    "the cat that killed ",
+    "the dog that worried ",
+    "the cow with the crumpled horn that tossed ",
+    "the maiden all forlorn that milked ",
+    "the man all tattered and torn that kissed ",
+    "the priest all shaven and shorn that married ",
+    "the rooster that crowed in the morn that woke ",
+    "the farmer sowing his corn that kept ",
+    "the horse and the hound and the horn that belonged to "
   )
 
   def recite(firstVerseIndex: Int, lastVerseIndex: Int): String =
-    def reciteAcc(firstVerseIndex: Int, lastVerseIndex: Int, acc: String): String =
-      if firstVerseIndex > lastVerseIndex then acc
+    def reciteAcc(firstVerseIndex: Int, lastVerseIndex: Int, acc: String, lineIndex: Int): String =
+      if firstVerseIndex > lastVerseIndex then s"$acc\n" // end of the song
+      else if lineIndex == 0 then reciteAcc(firstVerseIndex + 1, lastVerseIndex, s"$acc\n", firstVerseIndex + 1) // end of one paragraph
       else
-        var result = s"This is ${phrases(firstVerseIndex-1)}"
-        for
-          index <- Range(firstVerseIndex -1, 0, -1)
-        do
-          result = result.++(s" ${phrases(index - 1)}")
-        reciteAcc(firstVerseIndex + 1, lastVerseIndex, acc ++ s"$result\n")
-    reciteAcc(firstVerseIndex, lastVerseIndex, "") + "\n"
+        val phrase = phrases(lineIndex - 1)
+        val result = if firstVerseIndex == lineIndex then "This is " + phrase else phrase
+        reciteAcc(firstVerseIndex, lastVerseIndex, acc + result, lineIndex - 1)
+    reciteAcc(firstVerseIndex, lastVerseIndex, "", firstVerseIndex)
