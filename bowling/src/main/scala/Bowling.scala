d295eb4f577b9afa137f365a27bc32f6b243da3b
Clara Gaset <claragaset4@gmail.com>
2022-04-28 18:03:41 +0200

    WIP: refactor

--- bowling/src/main/scala/Bowling.scala
+++ bowling/src/main/scala/Bowling.scala
@@ -1,15 +1,23 @@
-class Bowling(points: Int, previousScore: Int, rollTurn: Int, nextHasToBeTwice: Boolean):
+class Bowling2(points: Int, previousScore: Int, rollTurn: Int, nextHasToBeTwice: Boolean)
 
-  def this() = this(0, 0, 1, false)
+class Bowling(scores: List[Int] = List.empty):
+
+//  val scores: List[Int] = new Array[Int]();
+
+//  def this() = this(0, 0, 1, false)
+
+//  def roll2(score: Int): Bowling2 =
+//    if previousScore == 10 && nextHasToBeTwice then
+//      return new Bowling(points + score*2, score, rollTurn + 1, true)
+//    if score == 10 then
+//      return new Bowling(points + score, score, rollTurn + 2, true)
+//    if rollTurn % 2 == 0 && score + previousScore == 10 then return new Bowling(points + score, score, rollTurn + 1, rollTurn != 20)
+//    if nextHasToBeTwice then return new Bowling(points + score*2, score, rollTurn + 1, false)
+//    else return new Bowling(points + score, score, rollTurn + 1, false)
 
   def roll(score: Int): Bowling =
-    if previousScore == 10 && nextHasToBeTwice then
-      return new Bowling(points + score*2, score, rollTurn + 1, true)
-    if score == 10 then
-      return new Bowling(points + score, score, rollTurn + 2, true)
-    if rollTurn % 2 == 0 then
-      if score + previousScore == 10 then return new Bowling(points + score, score, rollTurn + 1, rollTurn != 20)
-    if nextHasToBeTwice then return new Bowling(points + score*2, score, rollTurn + 1, false)
-      else return new Bowling(points + score, score, rollTurn + 1, false)
+    new Bowling(scores :+ score);
 
-  def score(): Either[Boolean, Int] = Right(points)
+  def score(): Either[Boolean, Int] =
+    val result = scores.sum()
+    Right(result)
